b348dde0fe75e0faa17a5737ba6847cb6b9619e4
sangeetha <sangeetha@gamail.com>
2024-05-02 12:25:49 +0000

    character-counter

---
+++ src/CharacterCounter/styledComponents.js
@@ -0,0 +1,68 @@
+import styled from './styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+`
+export const LeftPanel = styled.div`
+  background-color: #ffc533;
+  height: 100vh;
+`
+export const InfoCardContainer = styled.div`
+  background-color: #ffbf1f;
+  padding: 20px;
+`
+export const Info = styled.h1`
+  font-weight: bold;
+  color: #334155;
+  font-family: 'Roboto';
+  line-height: 50px;
+  font-size: 32px;
+`
+export const UserInputsList = styled.div`
+  background-color: #0f172a;
+  height: 100vh;
+  padding: 20px;
+  width: 50%;
+`
+export const RightPanel = styled.div`
+  background-color: #0f172a;
+  height: 100vh;
+  padding: 20px;
+  width: 50%;
+`
+export const CounterHeading = styled.h1`
+  font-size: 32px;
+  color: #334155;
+  font-family: 'Roboto';
+  line-height: 50px;
+`
+export const AddInputContainer = styled.div`
+  display: flex;
+`
+export const Input = styled.input`
+  height: 35px;
+  width: 100%;
+  padding-left: 15px;
+  color: #475569:
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+`
+export const AddInputButton = styled.button`
+  height: 35px;
+  width: 100%;
+  padding-left: 15px;
+  color: #ffc533:
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+`
+export const EmptyImage = styled.img`
+  height: 400px;
+  width: 100%;
+  padding: 50px;
+  margin-top: 60px;
+`
